562d9f51cb7ee9d30dc7655757efe2202b84ffc8
akdubov <akdubov@yandex.ru>
2020-05-20 22:53:04 +0300

    some fixes #5.3

--- rest-server-service/src/main/java/ru/vtb/dis/pilot01/ServerServiceController.java
+++ rest-server-service/src/main/java/ru/vtb/dis/pilot01/ServerServiceController.java
@@ -2,7 +2,9 @@ package ru.vtb.dis.pilot01;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.vtb.dis.pilot01.structure.CommonServerResponse;
@@ -15,13 +17,31 @@ public class ServerServiceController {
 
     @GetMapping("/getServiceResult")
     public CommonServerResponse getServiceResult(
+            @RequestHeader HttpHeaders headers,
             @RequestParam(value = "requestUID", defaultValue = "noUID") String requestUID,
             @RequestParam(value = "callerServiceName", defaultValue = "namelessService") String callerServiceName) {
+        return processRequest(headers, requestUID, callerServiceName);
+    }
 
+    @GetMapping("/reserved/getServiceResult")
+    public CommonServerResponse getServiceResultReserved(
+            @RequestHeader HttpHeaders headers,
+            @RequestParam(value = "requestUID", defaultValue = "noUID") String requestUID,
+            @RequestParam(value = "callerServiceName", defaultValue = "namelessService") String callerServiceName) {
+        return processRequest(headers, requestUID, callerServiceName);
+    }
+
+    private CommonServerResponse processRequest(HttpHeaders headers, String requestUID, String callerServiceName) {
         String responseUID = UUID.randomUUID().toString();
         String resultText = String.format("'%s': a response '%s' has been generated for the calling '%s' service",
                 requestUID,responseUID, callerServiceName);
-        LOGGER.info("{}", resultText);
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.info("{}\n\t\theader: {}", resultText, headers != null ? headers.toString() : "NULL");
+        } else {
+            LOGGER.info("{}", resultText);
+        }
         return new CommonServerResponse(requestUID, responseUID, resultText);
     }
+
 }
